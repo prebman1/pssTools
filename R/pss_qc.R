3b29aa0ee25f9f6c8b2162b7cb98547907e60630
prebman1 <prebman1@jh.edu>
2022-01-09 17:24:11 -0500

    fixed logs timestamp

--- R/pss_qc.R
+++ R/pss_qc.R
@@ -14,6 +14,9 @@
 #' cases <- redcap_read(redcap_uri = url, "token"=token, events = redcap_events)$data
 #' qc <- pss_qc(cases)
 pss_qc <- function(cases, logs, data_dictionary, qc_rules, form_mapping, write_files = FALSE) {
+  #remove seconds from log timestamp
+  logs$timestamp <- format(logs$timestamp, "%Y-%m-%d %H:%M")
+
   #clean data_dictionary file
   data_dictionary <- data_dictionary[,c(1,2,4,12,18)]
   data_dictionary <- data_dictionary[-c(which(data_dictionary$Variable...Field.Name == "survey_admin_notes")),]
