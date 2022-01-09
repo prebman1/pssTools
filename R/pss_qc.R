bf17538f4ad7ab070e221707e704e8075d939547
prebman1 <prebman1@jh.edu>
2022-01-09 13:49:19 -0500

    uncommented next three lines of code

--- R/pss_qc.R
+++ R/pss_qc.R
@@ -266,15 +266,15 @@ pss_qc <- function(cases, logs, data_dictionary, qc_rules) {
   rmarkdown::render("Missingness.Rmd", output_file = paste0("Missingness - ", output_time))
 
 
-  # ##### Missing data for select initial_contact_fo vars
-  # initial_cont_miss_vars <- c("iden_date_mdy", "contact_dob", "reg_phone", "teacher_school_location",
-  #                             "teacher_school_other", "parent_school_location", "parent_school_other",
-  #                             "numchild_b", "randomization_child_num", "dob_child", "contact_decline_other",
-  #                             "contact_ineligible_other", "withdrew_reason", "exitstatus_other", "name_child1",
-  #                             "age_child1", "name_child2", "age_child2", "name_child3", "age_child3", "name_child4",
-  #                             "age_child4", "name_child5", "age_child5", "name_child6", "age_child6")
-  # form_completed <- qc_cases %>% filter(redcap_event_name == "initial_contact_fo" & (!is.na(caregiver) | !is.na(school_employee) | !is.na(adult)))
-  # initial_cont_data_dict <- data_dictionary %>% filter(var %in% initial_cont_miss_vars)
+  ##### Missing data for select initial_contact_fo vars
+  initial_cont_miss_vars <- c("iden_date_mdy", "contact_dob", "reg_phone", "teacher_school_location",
+                              "teacher_school_other", "parent_school_location", "parent_school_other",
+                              "numchild_b", "randomization_child_num", "dob_child", "contact_decline_other",
+                              "contact_ineligible_other", "withdrew_reason", "exitstatus_other", "name_child1",
+                              "age_child1", "name_child2", "age_child2", "name_child3", "age_child3", "name_child4",
+                              "age_child4", "name_child5", "age_child5", "name_child6", "age_child6")
+  form_completed <- qc_cases %>% filter(redcap_event_name == "initial_contact_fo" & (!is.na(caregiver) | !is.na(school_employee) | !is.na(adult)))
+  initial_cont_data_dict <- data_dictionary %>% filter(var %in% initial_cont_miss_vars)
   # for(i in initial_cont_miss_vars) {
   #   for(r in 1:nrow(form_completed)) {
   #     is_missing <- is.na(form_completed[,which(colnames(form_completed) == i)])[r]
